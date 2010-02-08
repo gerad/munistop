ae4f794ad5ba7e96d9576ae02b11e7121d7dc811
Gerad Suyderhoud <gerads@gmail.com>
2010-02-07 19:37:41 -0800

    fix a ton of iphone specific bugs

--- static/js/json2.js
+++ static/js/json2.js
@@ -159,7 +159,7 @@
 // Create a JSON object only if one does not already exist. We create the
 // methods in a closure to avoid creating global variables.
 
-JSON || (function () {
+this.JSON || (function () {
     this.JSON = {};
 
     function f(n) {
